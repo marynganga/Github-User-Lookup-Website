bc71ceb979a13d4c42c75401aecfc25b8dac5ba8
marynganga <marynganga.mw@gmail.com>
2017-09-09 13:44:33 +0300

    Display user's public repos. 1. Add a .get() function to retrieve repo's name and description from the user's public repos url. 2. Append the repo's name and description to the webpage.

--- build/js/app.js
+++ build/js/app.js
@@ -10,10 +10,25 @@ exports.getRepos = function(name){
     console.log(response.following);
     console.log(response.public_repos);
     console.log(response.url);
+    console.log(response.repos_url);
+
+
 $('.userName').append('<p>The username entered is: '+name + '</p>'
 					 +'<p>The user\'s name is: '+response.name + '</p>');
-// $('.publicRepos').append('<p>The username entered is:'+name + '</p>');
 
+	$.get(response.repos_url).then(function(repos){
+		repos.forEach(function(repo){
+		console.log(repos[0].name);
+		console.log(repos[1].description);
+		$('.publicRepos').append('<p>The repository\'s name: '+repos[0].name + '</p>'
+					 +'<p>The repository\'s description: '+repos[1].description + '</p>');
+
+	}).fail(function(error){
+    console.log(error.reposJSON.message);
+
+	})
+
+});
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
