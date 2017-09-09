5b5a4e66ba846f62a72f3ff94ee56f75ba650865
marynganga <marynganga.mw@gmail.com>
2017-09-09 12:54:34 +0300

    Display entered username on page.

--- build/js/app.js
+++ build/js/app.js
@@ -16,11 +16,20 @@ var getRepos = require('./../js/lookup.js').getRepos;
 
 $(document).ready(function(){
 
-$('#setAlarmButton').click(function(){
+$('#setAlarmButton').click(function(event){
+	event.preventDefault();
+	var name = $('#name').val();
+	console.log(name);
+	$('.userName').append('<p>The username entered is:'+name + '</p>');
+
+
+
+
+
 	getRepos();
-})
+});
 
 
 
-})
+});
 },{"./../js/lookup.js":2}]},{},[3]);
--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -3,10 +3,19 @@ var getRepos = require('./../js/lookup.js').getRepos;
 
 $(document).ready(function(){
 
-$('#setAlarmButton').click(function(){
+$('#setAlarmButton').click(function(event){
+	event.preventDefault();
+	var name = $('#name').val();
+	console.log(name);
+	$('.userName').append('<p>The username entered is:'+name + '</p>');
+
+
+
+
+
 	getRepos();
-})
+});
 
 
 
-})
+});
