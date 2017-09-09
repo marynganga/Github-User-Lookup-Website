9c004328b0a164cdc8e40411b12bf9550e5c9f20
marynganga <marynganga.mw@gmail.com>
2017-09-09 15:21:59 +0300

    Number all the displayed public repos.

--- build/js/app.js
+++ build/js/app.js
@@ -8,25 +8,26 @@ exports.getRepos = function(name){
     console.log(response);
     console.log(response.followers);
     console.log(response.following);
-    console.log(response.public_repos);
+    // console.log(response.public_repos);
     console.log(response.url);
-    console.log(response.repos_url);
+    // console.log(response.repos_url);
 
 
 $('.userName').append('<p>The username entered is: '+name + '</p>'
 					 +'<p>The user\'s name is: '+response.name + '</p>');
 
 	$.get(response.repos_url).then(function(repos){
+
 		for (var i = 0; i <= response.public_repos-1; i++) {
-		console.log(repos[i].name);
-		console.log(repos[i].description);
-		$('.publicRepos').append('<p>The repository\'s name: '+repos[i].name + '</p>'
+		// console.log(repos[i].name);
+		// console.log(repos[i].description);
+		var index= i+1;
+	// for (numbers; numbers <= response.public_repos; numbers++) {
+
+		$('.publicRepos').append('<p>'+index+'. The repository\'s name: '+repos[i].name + '</p>'
 					 +'<p>The repository\'s description: '+repos[i].description + '</p>');
-
+// }
 	}
-
-	}).fail(function(error){
-    console.log(error.reposJSON.message);
 
 	})
 }).fail(function(error){
