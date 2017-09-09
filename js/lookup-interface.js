50f9c823302235e309c28385697f494ec4986bf1
marynganga <marynganga.mw@gmail.com>
2017-09-09 13:12:49 +0300

    Display the entered user's full names. 1. Append the user's full names to the webpage.

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -3,11 +3,10 @@ var getRepos = require('./../js/lookup.js').getRepos;
 
 $(document).ready(function(){
 
-$('#setAlarmButton').click(function(event){
+$('#submitName').click(function(event){
 	event.preventDefault();
 	var name = $('#name').val();
 	console.log(name);
-	$('.userName').append('<p>The username entered is:'+name + '</p>');
 	getRepos(name);
 });
 
--- js/lookup.js
+++ js/lookup.js
@@ -3,6 +3,14 @@ var apiKey = require('./../.env').apiKey;
 exports.getRepos = function(name){
   $.get('https://api.github.com/users/'+name+'?access_token=' + apiKey).then(function(response){
     console.log(response);
+    console.log(response.followers);
+    console.log(response.following);
+    console.log(response.public_repos);
+    console.log(response.url);
+$('.userName').append('<p>The username entered is: '+name + '</p>'
+					 +'<p>The user\'s name is: '+response.name + '</p>');
+// $('.publicRepos').append('<p>The username entered is:'+name + '</p>');
+
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
