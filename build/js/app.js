bf95919706173f4adb78166e5187bf95524730b5
marynganga <marynganga.mw@gmail.com>
2017-09-09 15:57:53 +0300

    Display the user's github url.

--- build/js/app.js
+++ build/js/app.js
@@ -6,15 +6,16 @@ var apiKey = require('./../.env').apiKey;
 exports.getRepos = function(name){
   $.get('https://api.github.com/users/'+name+'?access_token=' + apiKey).then(function(response){
     console.log(response);
-    console.log(response.followers);
-    console.log(response.following);
+    // console.log(response.followers);
+    // console.log(response.following);
     // console.log(response.public_repos);
-    console.log(response.url);
-    console.log(response.avatar_url);
+    console.log(response.html_url);
+    // console.log(response.avatar_url);
 
 
-$('.userName').append('<p>The username entered is: '+name + '</p>'
-					 +'<p>The user\'s name is: '+response.name + '</p>'
+$('.userName').append('<p>Username: '+name + '</p>'
+					 +'<p>Full Name: '+response.name + '</p>'
+					 +'<p>Github Url: '+'<a target="_blank" href="'+response.html_url + '">'+response.html_url+'</a></p>'
 					 +'<p>Following: '+response.following + '</p>'
 					 +'<p>Followers: '+response.followers + '</p>'
 					 +'<img src='+response.avatar_url+' alt="Image of user" />');
